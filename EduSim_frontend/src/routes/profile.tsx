02eb5848f6ad5c36ccd01d7ab5de6b9ef084abfb
satishx9 <thadelasatish@gmail.com>
2026-05-08 16:16:14 +0530

    Removed frontend folder from repository

--- EduSim_frontend/src/routes/profile.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Stub } from "./my-simulations";
-export const Route = createFileRoute("/profile")({
-  component: () => Stub("Profile", "Manage your student profile."),
-});
